c1067ce37935a8723daee0d4a7346bca4382b895
FrontEndDevT <mailtoprograms@gmail.com>
2015-11-16 15:55:10 +0100

    getting data from json and list them in view added

--- js/application.js
+++ js/application.js
@@ -1,3 +1,16 @@
 'use strict'
 
-var app = angular.module( 'app', [] );
+var app = angular.module( 'app', [ 'ngRoute' ] );
+
+app.config( [ '$routeProvider', function( $routeProvider ) {
+    
+}]);
+
+app.controller( 'products' , [ '$scope', '$http', function( $scope, $http ) {
+    $http.get( 'model/products.json' ).
+    success( function( data ) {
+        $scope.products = data;
+    }).error( function() {
+        console.log('Błąd pobierania pliku products.json');
+    });
+}]);
