a329ccb2d58ac1b60c3cd137713734224292acee
FrontEndDevT <mailtoprograms@gmail.com>
2015-11-19 18:10:42 +0100

    deleting product and list users feature added

--- js/application.js
+++ js/application.js
@@ -4,18 +4,23 @@ var app = angular.module( 'app', [ 'ngRoute', 'ctrls' ] );
 
 app.config( [ '$routeProvider', '$httpProvider', function( $routeProvider, $httpProvider ) {  
     $routeProvider.when( '/products', {
-        controller : 'products',
+        controller : 'ProductsCtrl',
         templateUrl : 'partials/products.html'
     });
 
     $routeProvider.when( '/product/edit/:id', {
-        controller : 'editProductCtrl',
+        controller : 'EditProductCtrl',
         templateUrl : 'partials/product-edit.html'
     });
 
     $routeProvider.when( '/product/add', {
-        controller : 'addProductCtrl',
+        controller : 'AddProductCtrl',
         templateUrl : 'partials/product-add.html'
+    });
+
+    $routeProvider.when( '/users', {
+        controller : 'UsersCtrl',
+        templateUrl : 'partials/users.html'
     });
 
     $routeProvider.otherwise({
--- js/controllers.js
+++ js/controllers.js
@@ -2,16 +2,27 @@
 
 var ctrls = angular.module( 'ctrls', [ 'ngRoute' ] );
 
-ctrls.controller( 'products' , [ '$scope', '$http', function( $scope, $http ) {
+ctrls.controller( 'NavigationCtrl', [ '$scope', '$location', function( $scope, $location ) {
+    $scope.isActive = function ( path ) {
+        return $location.path() === path;
+    };
+}]);
+
+ctrls.controller( 'ProductsCtrl' , [ '$scope', '$http', function( $scope, $http ) {
     $http.get( 'model/products.json' ).
     success( function( data ) {
         $scope.products = data;
     }).error( function() {
         console.log('Błąd pobierania pliku products.json');
     });
+
+    $scope.deleteProduct = function ( product, index ) {
+
+    	$scope.products.splice( index, 1 );
+    };
 }]);
 
-ctrls.controller( 'editProductCtrl', [ '$scope', '$http', '$routeParams', function( $scope, $http, $routeParams ) {
+ctrls.controller( 'EditProductCtrl', [ '$scope', '$http', '$routeParams', function( $scope, $http, $routeParams ) {
 	$http.get( 'model/products.json' ).
     success( function( data ) {
         $scope.product = data[$routeParams.id];
@@ -26,9 +37,18 @@ ctrls.controller( 'editProductCtrl', [ '$scope', '$http', '$routeParams', functi
     };
 }]);
 
-ctrls.controller( 'addProductCtrl', [ '$scope', '$http', function( $scope, $http ) {
+ctrls.controller( 'AddProductCtrl', [ '$scope', '$http', function( $scope, $http ) {
 	$scope.addProduct = function () {
 
     	console.log( $scope.product );
     };
+}]);
+
+ctrls.controller( 'UsersCtrl', [ '$scope', '$http', function( $scope, $http ) {
+    $http.get( 'model/users.json' ).
+    success( function( data ) {
+        $scope.users = data;
+    }).error( function() {
+        console.log('Błąd pobierania pliku users.json');
+    });
 }]);
